439cebe98bbcdbfe447f99bf41f57cff67d96ea2
Rodolpho Beckman <rodolpho.beckman@gmail.com>
2025-09-13 16:53:23 +0000

    MUDAR O NOME DO APP PARA: ClosetPic

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,20 +7,15 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        <div className="mr-4 hidden md:flex">
-          <a href="/" className="flex items-center space-x-2">
+        <div className="flex-1">
+          <a href="/" className="flex items-center space-x-2 w-fit mx-auto">
             <Camera className="h-6 w-6 bg-gradient-to-r from-primary to-purple-500 text-transparent bg-clip-text" />
-            <span className="font-bold sm:inline-block bg-gradient-to-r from-primary to-purple-500 text-transparent bg-clip-text drop-shadow-[0_0_0.3rem_#ffffff40]">ImageKeep</span>
+            <span className="font-bold sm:inline-block bg-gradient-to-r from-primary to-purple-500 text-transparent bg-clip-text drop-shadow-[0_0_0.3rem_#ffffff40]">ClosetPic</span>
           </a>
         </div>
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <div className="w-full flex-1 md:w-auto md:flex-none">
-            {/* Search can go here if needed */}
-          </div>
-          <nav className="flex items-center">
+        <nav className="flex items-center">
             <ThemeToggle />
-          </nav>
-        </div>
+        </nav>
       </div>
     </header>
   );
