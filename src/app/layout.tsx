019dae37b51b575e24697371c0c5f28f29f6e84d
Rodolpho Beckman <rodolpho.beckman@gmail.com>
2025-09-15 15:04:34 +0000

    CRIAR UMA PASTA PUBLIC ONDE POSSA COLOCAR UMA IMAGEM EM PNG LA PARA QUE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,16 @@ export default function RootLayout({
       <head>
         <title>ClosetPic</title>
         <meta name="description" content="Upload, categorize, and search your images." />
+        <meta name="application-name" content="ClosetPic" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="ClosetPic" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#8B5CF6" />
+
+        <link rel="manifest" href="/manifest.webmanifest" />
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
