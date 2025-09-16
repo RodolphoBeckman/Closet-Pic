7a2e726b9449a4d3e0acd31a269a113ec5489185
Rodolpho Beckman <rodolpho.beckman@gmail.com>
2025-09-16 12:45:09 +0000

    ainda nao redireciona para a pagina HOme. ele carrega e volta pra pagina

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
-
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { getSession } from '@/lib/session';
 
 // Rotas que não requerem autenticação
@@ -10,15 +9,15 @@ export async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isPublicRoute = publicRoutes.includes(path);
   
-  // Obtém a sessão diretamente do cookie, pois o `getSession` do lado do servidor funciona aqui
+  // Obtém a sessão diretamente do cookie.
   const session = await getSession();
 
-  // REGRA 1: Redirecionar para a página principal se o utilizador estiver logado e tentar aceder a uma rota pública
+  // REGRA 1: Redirecionar para a página principal se o utilizador estiver logado e tentar aceder a uma rota pública.
   if (isPublicRoute && session?.email) {
     return NextResponse.redirect(new URL('/', req.nextUrl));
   }
 
-  // REGRA 2: Redirecionar para o login se o utilizador não estiver logado e tentar aceder a uma rota protegida
+  // REGRA 2: Redirecionar para o login se o utilizador não estiver logado e tentar aceder a uma rota protegida.
   if (!isPublicRoute && !session?.email) {
     return NextResponse.redirect(new URL('/login', req.nextUrl));
   }
@@ -33,4 +32,4 @@ export async function middleware(req: NextRequest) {
 export const config = {
   // Executa o middleware em todas as rotas, exceto nas de API, ficheiros estáticos, imagens e favicon.
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-}
+};
