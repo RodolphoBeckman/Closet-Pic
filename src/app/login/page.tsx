ba7a9571516b8bc6deae48b034549c1c672e6191
Rodolpho Beckman <rodolpho.beckman@gmail.com>
2025-09-16 12:51:42 +0000

    ainda nao redireciona para a pagina HOme. ele faz o login e volta pra pa

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { useState, useTransition } from 'react';
+import { useTransition } from 'react';
 
 import { Button } from '@/components/ui/button';
 import {
